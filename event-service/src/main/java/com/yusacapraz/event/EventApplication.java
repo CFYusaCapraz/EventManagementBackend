cac942c15982965bf54111412954ca0f14cc7d14
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-10-14 21:40:41 +0300

    Event service created

---
+++ event-service/src/main/java/com/yusacapraz/event/EventApplication.java
@@ -0,0 +1,13 @@
+package com.yusacapraz.event;
+
+/**
+ * Hello world!
+ *
+ */
+public class EventApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
