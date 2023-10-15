ae88f64b212b43c1a9aa159c9262cc9cba3326ff
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-10-15 12:24:02 +0300

    Load balanced web client bean Not using rest template as suggested in the spring docs

--- gateway-service/src/main/java/com/yusacapraz/gateway/config/ApplicationConfig.java
+++ gateway-service/src/main/java/com/yusacapraz/gateway/config/ApplicationConfig.java
@@ -1,14 +1,16 @@
 package com.yusacapraz.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ApplicationConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 }
