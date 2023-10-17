170d0c82c481bfb9ce992dde79f3cdab2391b051
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-10-18 01:12:45 +0300

    Response dto used in the service layer

--- auth-service/src/main/java/com/yusacapraz/auth/service/AuthenticationService.java
+++ auth-service/src/main/java/com/yusacapraz/auth/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package com.yusacapraz.auth.service;
 
+import com.yusacapraz.auth.model.APIResponse;
 import com.yusacapraz.auth.model.DTOs.LoginDTO;
+import com.yusacapraz.auth.model.DTOs.LoginResponseDTO;
 import com.yusacapraz.auth.model.DTOs.ValidateDTO;
+import com.yusacapraz.auth.model.DTOs.ValidateResponseDTO;
 import com.yusacapraz.auth.security.jwt.JwtTokenProvider;
 import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +24,7 @@ public class AuthenticationService {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-    public ResponseEntity<?> login(LoginDTO loginDTO) {
+    public ResponseEntity<APIResponse<?>> login(LoginDTO loginDTO) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
@@ -29,25 +33,35 @@ public class AuthenticationService {
                     )
             );
             String token = jwtTokenProvider.generateToken(authentication);
-            return ResponseEntity.ok(token);
-        } catch (Exception e) {
-            return ResponseEntity.notFound().build();
+
+            LoginResponseDTO responseDTO = LoginResponseDTO.builder()
+                    .token(token)
+                    .build();
+            APIResponse<LoginResponseDTO> response = APIResponse.successWithData(responseDTO, "Successfully logged in.");
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (AuthenticationException ex) {
+            APIResponse<Object> response = APIResponse.error("Login unsuccessful.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
         }
     }
 
-    public ResponseEntity<?> validateToken(ValidateDTO validateDTO) {
+    public ResponseEntity<APIResponse<?>> validateToken(ValidateDTO validateDTO) {
         try {
             if (jwtTokenProvider.validateToken(validateDTO.getToken())) {
                 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
                 if (!authentication.isAuthenticated()) {
                     return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-
                 }
-                return ResponseEntity.ok().build();
+                ValidateResponseDTO responseDTO = ValidateResponseDTO.builder()
+                        .build();
+                APIResponse<ValidateResponseDTO> response = APIResponse.successWithData(responseDTO, "Token validated.");
+                return ResponseEntity.status(HttpStatus.OK).body(response);
             }
             return ResponseEntity.internalServerError().build();
         } catch (ExpiredJwtException e) {
-            return ResponseEntity.badRequest().body("Expired token!");
+            APIResponse<Object> response = APIResponse.error("Expired token!");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 }
