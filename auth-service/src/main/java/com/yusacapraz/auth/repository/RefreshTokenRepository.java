7d7c7958196bc7935168fe4f632df0e04bfe397c
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-10-19 01:17:15 +0300

    Method to find refresh token by refresh token UUID value

--- auth-service/src/main/java/com/yusacapraz/auth/repository/RefreshTokenRepository.java
+++ auth-service/src/main/java/com/yusacapraz/auth/repository/RefreshTokenRepository.java
@@ -4,6 +4,10 @@ import com.yusacapraz.auth.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findRefreshTokenByRefreshToken(UUID refreshToken);
 }
