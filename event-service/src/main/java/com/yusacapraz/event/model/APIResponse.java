fcd070a38fdd43732eebc2a4176dc13ec6bed710
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-10-22 17:20:21 +0300

    API Response class implemented in event service

---
+++ event-service/src/main/java/com/yusacapraz/event/model/APIResponse.java
@@ -0,0 +1,40 @@
+package com.yusacapraz.event.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class APIResponse<T> {
+    private boolean status;
+    private String message;
+    private T data;
+
+    public static <T> APIResponse<T> success(String message) {
+        APIResponse<T> response = new APIResponse<>();
+        response.setStatus(true);
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T> APIResponse<T> successWithData(T data, String message) {
+        APIResponse<T> response = new APIResponse<>();
+        response.setStatus(true);
+        response.setMessage(message);
+        response.setData(data);
+        return response;
+    }
+
+    public static <T> APIResponse<T> error(String message) {
+        APIResponse<T> response = new APIResponse<>();
+        response.setStatus(false);
+        response.setMessage(message);
+        return response;
+    }
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public T getData() {
+        return data;
+    }
+}
